7f24285fc43d6b7165ecd9009f11ec594262510b
Diogo Matos <diogomatosemail@gmail.com>
2024-01-16 18:29:52 +0000

    instituicoes

--- Controller/Instituicao.cs
+++ Controller/Instituicao.cs
@@ -1,2 +1,36 @@
 
-public class Instituicao{}
+public class Instituicao {
+    private int id;
+    private string nome;
+    private string descricao;
+    private string logoPath;
+    private string link;
+
+    public Instituicao(int id, string nome, string descricao, string logoPath, string link) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.logoPath = logoPath;
+        this.link = link;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public string getNome() {
+        return nome;
+    }
+
+    public string getDescricao() {
+        return descricao;
+    }
+
+    public string getLogoPath() {
+        return logoPath;
+    }
+
+    public string getLink() {
+        return link;
+    }
+}
