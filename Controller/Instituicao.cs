9f0c7384a704181515d4de5201163041277c9f09
Manuel-Serrano22 <manser03@gmail.com>
2024-01-16 20:08:47 +0000

    Alterações na instituição

--- Controller/Instituicao.cs
+++ Controller/Instituicao.cs
@@ -1,17 +1,38 @@
 
+using System.Text;
+
 public class Instituicao {
     private int id;
     private string nome;
     private string descricao;
     private string logoPath;
     private string link;
+    private string iban;
+    private int idAdmin;
 
-    public Instituicao(int id, string nome, string descricao, string logoPath, string link) {
+    private int contadorInstituicaoes = InstituicaoDAO.size();
+
+    //Construtor para o get
+    public Instituicao(int id, string nome, string descricao, string logoPath, string link, string iban, int idAdmin) {
         this.id = id;
         this.nome = nome;
         this.descricao = descricao;
         this.logoPath = logoPath;
         this.link = link;
+        this.iban = iban;
+        this.idAdmin = idAdmin;
+    }
+    
+    //Construtor para criar
+    public Instituicao(string nome, string descricao, string logoPath, string link, string iban, int idAdmin)
+    {
+        this.id = ++contadorInstituicaoes;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.logoPath = logoPath;
+        this.link = link;
+        this.iban = iban;
+        this.idAdmin = idAdmin;
     }
 
     public int getId() {
@@ -32,5 +53,50 @@ public class Instituicao {
 
     public string getLink() {
         return link;
+    }
+
+    public string getIban()
+    {
+        return this.iban;
+    }
+
+    public int getIdAdmin()
+    {
+        return this.idAdmin;
+    }
+    
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.Append("Instituição:: {");
+        sb.Append(" IdInstituicao: ").Append(this.id);
+        sb.Append(" Nome: ").Append(this.nome);
+        sb.Append(" Descrição: ").Append(this.descricao);
+        sb.Append(" Hiperligação: ").Append(this.link);
+        sb.Append(" IBAN: ").Append(this.iban);
+        sb.Append(" Id Admin: ").Append(this.idAdmin).Append(" }");
+
+        return sb.ToString();
+    } 
+
+    public override bool Equals(object obj){
+        // Verifica se o objeto é o próprio objeto
+        if (obj == this)
+            return true;
+
+        // Verifica se o objeto é nulo ou pertence a uma classe diferente
+        if (obj == null || obj.GetType() != this.GetType())
+            return false;
+
+        // Faz a conversão segura do objeto para a classe Licitador
+        Instituicao other = (Instituicao)obj;
+
+        // Verifica a igualdade dos atributos
+        return (this.id == other.getId() &&
+                this.nome.Equals(other.getNome()) &&
+                this.descricao.Equals(other.getDescricao()) &&
+                this.link.Equals(other.link) &&
+                this.iban.Equals(other.iban) &&
+                this.idAdmin == other.getIdAdmin());
     }
 }
