e94c0c646024466ce6112188ba92412dc50f5abe
Bruno Silva <a100828@alunos.uminho.pt>
2024-01-18 20:30:28 +0000

    Notificacao

--- Controller/Notificacao.cs
+++ Controller/Notificacao.cs
@@ -4,29 +4,35 @@ namespace RhythmsOfGiving.Controller {
         private int idNotificacao;
         private string title;
         private string message;
+        private int idLicatdor;
         private DateTime date;
         private int tipo; //0-> ultrapassada, 1-> vencedora, 2-> perdedora Vale a pena Enum??
 
         private static int contadorId = NotificacaoDAO.size();
         
         //Constutor para criar
-        public Notificacao(int idNotificacao, string title, string message, DateTime date, int tipo)
+
+        public Notificacao(int idNotificacao, string title, string message, int idLicatdor, DateTime date, int tipo)
         {
             this.idNotificacao = idNotificacao;
             this.title = title;
             this.message = message;
+            this.idLicatdor = idLicatdor;
             this.date = date;
             this.tipo = tipo;
         }
 
-        public Notificacao(string message,string title, DateTime date, int tipo)
+        public Notificacao(string title, string message, int idLicatdor, DateTime date, int tipo)
         {
-            this.message = message;
+            this.idNotificacao = ++contadorId;
             this.title = title;
+            this.message = message;
+            this.idLicatdor = idLicatdor;
             this.date = date;
             this.tipo = tipo;
         }
-        
+
+
         public int getId()
         {
             return idNotificacao;
