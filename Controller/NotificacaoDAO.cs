115a3761a9b6f42163bcc786d3204287f8a9c305
Bruno Silva <a100828@alunos.uminho.pt>
2024-01-19 00:46:10 +0000

    Notificação Get

--- Controller/NotificacaoDAO.cs
+++ Controller/NotificacaoDAO.cs
@@ -1,4 +1,5 @@
 
+using System.Data.SqlClient;
 using RhythmsOfGiving.Controller;
 
 public class NotificacaoDAO{
@@ -18,10 +19,46 @@ public class NotificacaoDAO{
             return 0; // depois usar a query necessária
         }
 
-        public Notificacao get(int id){
-            //falta definir a lógica
+         public Notificacao get(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(DAOconfig.GetConnectionString()))
+            {
+                connection.Open();
+                try
+                {
+
+                    string query = "SELECT * FROM Notificação WHERE idNotificacao = @Id";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Id", id);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                Notificacao notification = new Notificacao(
+                                    Convert.ToInt32(reader["idNotificacao"]),
+                                    Convert.ToString(reader["titulo"]),
+                                    Convert.ToString(reader["mensagem"]),
+                                    Convert.ToInt32(reader["idLicitador"]),
+                                    Convert.ToDateTime(reader["dataHora"]),
+                                    Convert.ToInt32(reader["tipo"]));
+
+                                return notification;
+                            }
+                        }
+                    }
+                }
+                catch
+                {
+                    throw new NaoExistemNotificacao("Não existe notificação para o id=" + id);
+                }
+            }
+
             return null;
-        } 
+        }
+
 
         public Notificacao put (int id, Notificacao n)
         {
